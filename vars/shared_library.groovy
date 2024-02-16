ee6e3c69e048d1c9a859bdadf0c4c54eea678097
Abhijit2714 <abijitdhamne@gmail.com>
2024-02-16 17:38:48 +0530

    Update shared_library.groovy

--- vars/shared_library.groovy
+++ vars/shared_library.groovy
@@ -2,8 +2,8 @@ def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Company is ${config.companyname}."
 }
 
-def Call(Map param = [:]) {
-    sh "echo address is ${param.address}. city is ${param.city}."
+def warning(message) {
+    echo "WARNING: ${message}"
 }
 
 

b984823de4019d9a4cbab4bf3a0edb381ad1f15f
Abhijit2714 <abijitdhamne@gmail.com>
2024-02-16 14:41:35 +0530

    Update shared_library.groovy

--- vars/shared_library.groovy
+++ vars/shared_library.groovy
@@ -1,5 +1,3 @@
 def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-    echo "Application Name: ${config.application}"
-    echo "Application Version: ${config.version}"
+    sh "echo Hello ${config.name}. Company is ${config.comapanyname}."
 }
